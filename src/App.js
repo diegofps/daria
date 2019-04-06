7d3e990d7c09d388376beb023dbdfc398db97534
Diego Souza <diegofpsouza@gmail.com>
2019-04-06 10:58:31 -0300

    Step 1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  data = {
+    "Ivel Z3": {
+      manufacturer: "Ivasim",
+      year: 1969,
+      origin: "Croatia"
+    },
+    "Bally Astrocade": {
+      manufacturer: "Bally Consumer Products",
+      year: 1977,
+      origin: "USA"
+    },
+    "Sord M200 Smart Home Computer": {
+      manufacturer: "Sord Computer Corporation",
+      year: 1971,
+      origin: "Japan"
+    },
+    "Commodore 64": {
+      manufacturer: "Commodore",
+      year: 1982,
+      origin: "USA"
+    }
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <form>
+            <select>
+              <option>- pick a model -</option>
+              {Object.keys(this.data).map((key) => 
+                <option value='{key}' >{key} ({this.data[key].year})</option>
+              )}
+            </select>
+          </form>
+
         </header>
       </div>
     );
